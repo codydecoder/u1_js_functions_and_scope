02d2e4dcbf07d842bd304e3acf81a584ea2880c0
codydecoder <codycad1@yahoo.com>
2024-01-18 10:57:43 -0600

    Functions practice

---
+++ script.js
@@ -0,0 +1,17 @@
+//functions
+
+//if() {}
+//switch() {}
+//for() {}
+//while() {}
+
+//declare function
+//then call it
+
+//arrow function: creating variable that stores function
+/*const addNums = (num1, num2) => {
+    console.log(num1 + num2)
+}
+addNums(5, 7);
+*/
+
